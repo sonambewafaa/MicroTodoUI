8aa53c28b562ca2778ccb2e71c1dce185bd74567
SohitGupta344 <sohitg89@gmail.com>
2026-08-09 13:25:39 +0530

    integrate azure key vault with aks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -8,7 +8,7 @@ import config from './config'; // Import the config file with environment variab
 // Use the config to define API URLs
 const GET_TASKS_API_BASE_URL = "http://get-tasks-api.sohitguptadevops.online"; // Replace with your actual API base URL
 const DELETE_TASK_API_BASE_URL = "http://delete-tasks-api.sohitguptadevops.online"; // Replace with your actual API base URL
-const CREATE_TASK_API_BASE_URL = "http://create-tasks-api.sohitguptadevops.online"; // Replace with your actual API base URL
+const CREATE_TASK_API_BASE_URL = "http://add-task-api.sohitguptadevops.online"; // Replace with your actual API base URL
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
